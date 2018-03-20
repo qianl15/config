e5378deb802a33059a22910df846c3576e52fc01
Qian Li <liqian.cs@gmail.com>
2018-03-19 18:59:11 -0600

    Update scripts and add files

--- cloudlab/install_memtier.sh
+++ cloudlab/install_memtier.sh
@@ -8,6 +8,28 @@ git fetch
 # git checkout Arachne
 autoreconf -ivf
 ./configure
-make
-sudo make install
+make -j32
 
+# Install skewed one
+MEMTIER_DIR=${HOME}/memtier_benchmark_skew
+
+git clone https://github.com/PlatformLab/memtier_benchmark.git ${MEMTIER_DIR}
+cd ${MEMTIER_DIR}
+git fetch
+git checkout skewed
+ln -s ${HOME}/arachne-all arachne-all
+autoreconf -ivf
+./configure
+make -j32
+
+# Install synthetic benchmark
+MEMTIER_DIR=${HOME}/memtier_benchmark_synthetic
+
+git clone https://github.com/PlatformLab/memtier_benchmark.git ${MEMTIER_DIR}
+cd ${MEMTIER_DIR}
+git fetch
+git checkout synthetic
+ln -s ${HOME}/arachne-all arachne-all
+autoreconf -ivf
+./configure
+make -j32
