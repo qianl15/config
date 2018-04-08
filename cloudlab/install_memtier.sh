6e0c6f5c19661e0f9cfe2e67530ec6ba44e022f8
Qian Li <liqian.cs@gmail.com>
2018-04-08 01:28:16 -0600

    Update install_memtier

--- cloudlab/install_memtier.sh
+++ cloudlab/install_memtier.sh
@@ -10,26 +10,15 @@ autoreconf -ivf
 ./configure
 make -j32
 
-# Install skewed one
-MEMTIER_DIR=${HOME}/memtier_benchmark_skew
+# Install skewed and synthetic workload
+MEMTIER_DIR=${HOME}/memtier_benchmark_skewsyn
 
 git clone https://github.com/PlatformLab/memtier_benchmark.git ${MEMTIER_DIR}
 cd ${MEMTIER_DIR}
 git fetch
-git checkout skewed
+git checkout skewSynthetic
 ln -s ${HOME}/arachne-all arachne-all
 autoreconf -ivf
 ./configure
 make -j32
 
-# Install synthetic benchmark
-MEMTIER_DIR=${HOME}/memtier_benchmark_synthetic
-
-git clone https://github.com/PlatformLab/memtier_benchmark.git ${MEMTIER_DIR}
-cd ${MEMTIER_DIR}
-git fetch
-git checkout synthetic
-ln -s ${HOME}/arachne-all arachne-all
-autoreconf -ivf
-./configure
-make -j32
