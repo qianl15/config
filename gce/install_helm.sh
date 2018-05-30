8eb3ad9bd1f185fd17ebed5db0056258eb78e75d
Qian Li <liqian.cs@gmail.com>
2018-05-30 15:59:57 -0700

    Fix install_helm.sh to avoid rbac conflicts.

--- gce/install_helm.sh
+++ gce/install_helm.sh
@@ -4,4 +4,6 @@ curl -LO https://storage.googleapis.com/kubernetes-helm/helm-v2.7.0-linux-amd64.
 tar xzf helm-v2.7.0-linux-amd64.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin
 
-helm init
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller --upgrade
